1040e35d00a837ea9444c0fb081b9bb0b249b2b8
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-12 21:50:44 +0800

    modified prog02 batch 2

--- batch_2/print_not_equal_when_numbers_are_not_the_same.py
+++ batch_2/print_not_equal_when_numbers_are_not_the_same.py
@@ -1,6 +1,6 @@
 #input
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #result
 print("Not Equal") if num1 != num2 else print("Equal")
