8120b5c998515a73406cb869a75b3a5d34d165f8
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-12 21:23:30 +0800

    added comments

--- batch_1/identify_equal_numbers.py
+++ batch_1/identify_equal_numbers.py
@@ -1,3 +1,6 @@
+#input: two numbers
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+#result
 print("Equal") if num1 == num2 else print("Not Equal")
--- batch_1/print_the_sum_of_two_numbers.py
+++ batch_1/print_the_sum_of_two_numbers.py
@@ -1,3 +1,6 @@
+#inputs
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
+
+#prints result
 print(f"The sum is: {num1 + num2}")
