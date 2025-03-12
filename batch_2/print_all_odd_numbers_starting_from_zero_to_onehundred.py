d71213f61bd0d77c90c9687eb1422a94d1966130
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-12 22:09:16 +0800

    finished prog08 batch 2

---
+++ batch_2/print_all_odd_numbers_starting_from_zero_to_onehundred.py
@@ -0,0 +1,14 @@
+# Initialize the variable i to 0
+i = 0
+
+# Loop while i is less than or equal to 100
+while i <= 100:
+    # Check if i is an odd number
+    if i % 2 != 0:
+        # Print the odd number followed by a space
+        print(i, end=' ')
+    # Increment i by 1
+    i += 1
+
+# Print a newline character at the end
+print()
