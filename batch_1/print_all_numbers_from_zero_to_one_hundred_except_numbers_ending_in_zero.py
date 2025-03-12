d676a8091a2ef9fcc39d8aa324a67720ff332da9
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-03-12 21:42:21 +0800

    finished batch 1

---
+++ batch_1/print_all_numbers_from_zero_to_one_hundred_except_numbers_ending_in_zero.py
@@ -0,0 +1,8 @@
+
+#finds out which numbers end in zero. If the number ends in zero, it will not be printed.
+for i in range(101):
+        if i % 10 != 0:
+            print(i, end=' ')
+
+#result
+print()
