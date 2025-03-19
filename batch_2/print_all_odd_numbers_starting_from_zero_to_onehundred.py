0d87ab0ef908be638f6a10878795e0d98c104fbe
Cholo <113973461+Secchoo@users.noreply.github.com>
2025-03-19 20:22:20 +0800

    edit

--- batch_2/print_all_odd_numbers_starting_from_zero_to_onehundred.py
+++ batch_2/print_all_odd_numbers_starting_from_zero_to_onehundred.py
@@ -1,14 +1,14 @@
-# Initialize the variable i to 0
-i = 0
+# Initialize the variable odd_num to 0
+odd_num = 0
 
-# Loop while i is less than or equal to 100
-while i <= 100:
-    # Check if i is an odd number
-    if i % 2 != 0:
+# Loop while odd_num is less than or equal to 100
+while odd_num <= 100:
+    # Check if odd_num is an odd number
+    if odd_num % 2 != 0:
         # Print the odd number followed by a space
-        print(i, end=' ')
-    # Increment i by 1
-    i += 1
+        print(odd_num, end=' ')
+    # Increment odd_num variable by 1
+    odd_num += 1
 
 # Print a newline character at the end
 print()
